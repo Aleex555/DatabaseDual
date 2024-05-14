7dc1330d937c21087e4ec431ec39d6f166d59bde
Aleex555 <amartinezgonzalez.cf@iesesteveterradas.cat>
2024-05-14 15:47:24 +0200

    .

--- src/main/java/cat/iesesteveterradas/dbapi/endpoints/UsuarisLogin.java
+++ src/main/java/cat/iesesteveterradas/dbapi/endpoints/UsuarisLogin.java
@@ -2,7 +2,6 @@ package cat.iesesteveterradas.dbapi.endpoints;
 
 import java.util.Random;
 
-
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,29 +17,33 @@ import jakarta.ws.rs.core.Response;
 @Path("/usuari/login")
 public class UsuarisLogin {
     private static final Logger logger = LoggerFactory.getLogger(UsuarisDao.class);
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response loginUsuari(String jsonInput) {
-        
+
         try {
             JSONObject input = new JSONObject(jsonInput);
-            
+
             String email = input.optString("email", null);
             String contrasena = input.optString("contraseña", null);
             String tipo = input.optString("tipo", null);
 
             // Validación para 'email'
             if (email == null || email.trim().isEmpty()) {
-                return Response.status(Response.Status.BAD_REQUEST).entity("{\"status\":\"ERROR\",\"message\":\"Email requerido\"}").build();
+                return Response.status(Response.Status.BAD_REQUEST)
+                        .entity("{\"status\":\"ERROR\",\"message\":\"Email requerido\"}").build();
             }
 
             if (contrasena == null || contrasena.trim().isEmpty()) {
-                return Response.status(Response.Status.BAD_REQUEST).entity("{\"status\":\"ERROR\",\"message\":\"Contraseña requerida \"}").build();
+                return Response.status(Response.Status.BAD_REQUEST)
+                        .entity("{\"status\":\"ERROR\",\"message\":\"Contraseña requerida \"}").build();
             }
 
             if (tipo == null || tipo.trim().isEmpty()) {
-                return Response.status(Response.Status.BAD_REQUEST).entity("{\"status\":\"ERROR\",\"message\":\"Contraseña requerida \"}").build();
+                return Response.status(Response.Status.BAD_REQUEST)
+                        .entity("{\"status\":\"ERROR\",\"message\":\"Contraseña requerida \"}").build();
             }
 
             Propietario propietario = new Propietario();
@@ -50,29 +53,35 @@ public class UsuarisLogin {
 
             JSONObject userData = new JSONObject();
 
-            if(tipo.equals("propietario") ){
+            if (tipo.equals("propietario")) {
                 propietario = PropietarioDao.encontrarPropietarioPorEmailYContrasena(email, contrasena);
-                if (propietario == null){
-                    return Response.status(Response.Status.ACCEPTED).entity("{\"status\":\"ERROR\",\"message\":\"Propietario no encontrado\"}").build();
-                }else{
+                if (propietario == null) {
+                    return Response.status(Response.Status.ACCEPTED)
+                            .entity("{\"status\":\"ERROR\",\"message\":\"Propietario no encontrado\"}").build();
+                } else {
                     userData.put("id", propietario.getPropietarioID());
                 }
-            }else{
-                user  = UsuarisDao.encontrarUsuarioPorEmailYContrasena(email, contrasena);
-                if (user == null){
-                    return Response.status(Response.Status.ACCEPTED).entity("{\"status\":\"ERROR\",\"message\":\"Usuario no encontrado\"}").build();
-                }else{
+            } else {
+                user = UsuarisDao.encontrarUsuarioPorEmailYContrasena(email, contrasena);
+                if (user == null) {
+                    return Response.status(Response.Status.ACCEPTED)
+                            .entity("{\"status\":\"ERROR\",\"message\":\"Usuario no encontrado\"}").build();
+                } else {
                     userData.put("id", user.getUserID());
-                    userData.put("gmail",user.getEmail());
-                    userData.put("telefono",user.getTelefono());
-                    userData.put("nombre",user.getNombre());
-                    userData.put("url",user.geturlFotoPerfil());
+                    userData.put("gmail", user.getEmail());
+                    userData.put("telefono", user.getTelefono());
+                    userData.put("nombre", user.getNombre());
+                    if (user.geturlFotoPerfil() == null) {
+                        userData.put("url", " ");
+                    } else {
+                        userData.put("url", user.geturlFotoPerfil());
+                    }
+
                 }
             }
 
             jsonResponse.put("status", "OK");
             jsonResponse.put("message", "Credenciales correctas");
-            
 
             // Añadir el objeto "data" al JSON de respuesta
             jsonResponse.put("data", userData);
@@ -82,7 +91,8 @@ public class UsuarisLogin {
             return Response.ok(prettyJsonResponse).build();
         } catch (Exception e) {
             e.printStackTrace();
-            return Response.serverError().entity("{\"status\":\"ERROR\",\"message\":\"Error en hacer el login\"}"+e).build();
+            return Response.serverError().entity("{\"status\":\"ERROR\",\"message\":\"Error en hacer el login\"}" + e)
+                    .build();
         }
     }
 }
